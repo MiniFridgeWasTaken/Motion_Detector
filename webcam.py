0a3f3fac09078ccbb21bf13dd0250bb0900449a0
MiniFridge <125807080+MiniFridgeWasTaken@users.noreply.github.com>
2024-02-17 23:34:21 +0800

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -2,24 +2,26 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-if not cap.isOpened():
+if not cap.isOpened():  #troubleshoot
   print("404:Webcam not found")
   exit()
 
 first_frame = None
 
 while True:
-  ret,frame = cap.read()
-  gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+  ret,frame = cap.read() # getting info from webcam
+  gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)  # Gray scaling
   gray = cv.GaussianBlur(gray,(21,21),0)
 
-  if first_frame is None:
-    first_frame = gray
+  if first_frame is None: 
+    first_frame = gray # setting first frame to gray scale frame
     continue
 
-  delta_frame = cv.absdiff(first_frame,gray)
+  delta_frame = cv.absdiff(first_frame,gray) #comparing curent frame to first frame
   threshold_frame = cv.threshold(delta_frame,50,255,cv.THRESH_BINARY)[1]
   threshold_frame = cv.dilate(threshold_frame,None,iterations=5)
+
+  
 
   (cntr,_) = cv.findContours(threshold_frame.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
   
@@ -28,16 +30,16 @@ while True:
       continue
 
     (x,y,w,h) = cv.boundingRect(contour)
-    cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+    cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)  # drawing a rectangle around the changes
 
   if not ret:
-    print("No Capture")
+    print("No Capture")  # troubleshoot
     break
 
-  cv.imshow('Webcam',frame)
+  cv.imshow('Webcam',frame)   # window show webcam
 
 
-  if cv.waitKey(1) == ord("q"):
+  if cv.waitKey(1) == ord("q"):  # exiting the code
     break
 
 
